1226f3c956a02ea94d0d101ccd6d96ac2cd4ee58
mdyaseencs19 <60700271+mdyaseencs19@users.noreply.github.com>
2020-04-09 04:50:21 +0530

    Add files via upload

---
+++ Digital_Clock/script.js
@@ -0,0 +1,30 @@
+setInterval(()=>{
+    const clock = document.querySelector('.show');
+    let time = new Date();
+    let sec = time.getSeconds();
+    let min = time.getMinutes();
+    let hr = time.getHours();
+    let day = 'AM';
+
+    if(hr > 12){
+        day = 'PM';
+        hr = hr-12;
+    }
+
+    if(hr == 0){
+        hr = 12;
+    }
+
+    if(sec < 10){
+        sec = '0' + sec;
+    }
+
+    if(min < 10){
+        min = '0' + min;
+    }
+
+    if(hr < 9){
+        hr = '0' + hr;
+    }
+    clock.textContent = hr + ':' + min + ':' + sec + ' ' + day;  
+});
