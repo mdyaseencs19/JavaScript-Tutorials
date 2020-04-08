f68fc6eecfbd8ca75133f7b3d5b5680baa6fd9a8
Md Yaseen Ahmed <60700271+mdyaseencs19@users.noreply.github.com>
2020-04-09 05:00:37 +0530

    Create 02 - Variables.js

---
+++ 01 -- Basics/02 - Variables.js
@@ -0,0 +1,15 @@
+let name = 'Yaseen';    // name is of type `string`
+console.log(name);  // Yaseen
+console.log(typeof(name));  //It prints `string`
+let score = 103;    // score is of type `integer`
+let bonus = 20;
+console.log(score+bonus);   //123
+
+let s1 = 100;
+let s2 = '20';
+console.log(s1+s2); //10020
+
+let lastName = 'ahmed';
+console.log(name + ' ' + lastName);    //Yaseen ahmed
+let fullName = name+ '-' +lastName;
+console.log(fullName);  //Yaseen-ahmed
